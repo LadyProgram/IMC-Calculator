1440b51ed541284c0334370af7734f02e8a72434
lady.program <nymeriastark74@gmail.com>
2025-02-21 13:27:49 +0100

    First concept of the app

--- app/src/main/java/com/ladyprogram/imccalculator/MainActivity.kt
+++ app/src/main/java/com/ladyprogram/imccalculator/MainActivity.kt
@@ -3,16 +3,19 @@ package com.ladyprogram.imccalculator
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var heightEditText: EditText
     lateinit var weightEditText: EditText
     lateinit var calculationButton: Button
+    lateinit var resultTextView: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,12 +34,21 @@ class MainActivity : AppCompatActivity() {
         heightEditText = findViewById(R.id.heightEditText)
         weightEditText = findViewById(R.id.weightEditText)
         calculationButton = findViewById(R.id.calculationButton)
+        resultTextView = findViewById(R.id.resultTextView)
 
+        heightEditText.setText("180")
+        weightEditText.setText("70")
 
 
         calculationButton.setOnClickListener {
+            val height = heightEditText.text.toString().toFloat()
+            val weight = weightEditText.text.toString().toFloat()
+            val result = weight / (height / 100).pow(2)
+
             println("Altura: " + heightEditText.text.toString())
             println("Peso: " + weightEditText.text.toString())
+
+            resultTextView.text = "$result"
         }
     }
 }
